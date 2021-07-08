cbff1e49ff91e49e7a21d7fca20bc1bda2a24f4d
shadownetz <okwuzuchiagozie@gmail.com>
2021-07-08 18:28:57 +0100

    prep for heroku deployment

--- MTS/settings.py
+++ MTS/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -127,3 +130,5 @@ DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * (1024*1024)
 
 AUTH_USER_MODEL = 'home.User'
 LOGIN_URL = '/login'
+
+django_heroku.settings(locals())
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn MTS.wsgi --log-file -
